7ed86024be98fc6430f1994b85062d8c572921ec
Deepthi Gunturi <dgunturi@codeaurora.org>
2019-01-03 10:29:51 +0530

    Moving vendor_framework_compatibility_matrix.xml to core-utils
    Change-Id: Id9512b61f1cc0acf7ccff99291645dd624fbc675

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -103,7 +103,7 @@ DEVICE_MANIFEST_FILE := device/qcom/msm8937_64/manifest.xml
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/msm8937_64/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    device/qcom/common/vendor_framework_compatibility_matrix.xml
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 # default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
 
