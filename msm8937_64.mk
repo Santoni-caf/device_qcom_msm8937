7f396933262eda2b1f6831e294297d6b9d6b8187
Himanshu Agrawal <hagraw@codeaurora.org>
2019-06-19 11:03:19 +0530

    msm8937_64: Compilation fixes for newer SPs.
    Change-Id: I020c4974da362d9b75da0bd3922894ca27cd6b48

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -395,6 +395,11 @@ endif
 
 TARGET_MOUNT_POINTS_SYMLINKS := false
 
+ifeq ($(strip $(TARGET_KERNEL_VERSION)), 3.18)
+PRODUCT_HOST_PACKAGES += \
+        AWBTestApp
+endif
+
 SDM660_DISABLE_MODULE := true
 
 # When AVB 2.0 is enabled, dm-verity is enabled differently,
@@ -414,7 +419,7 @@ endif
 
 ifeq ($(strip $(TARGET_KERNEL_VERSION)), 3.18)
     # Enable extra vendor libs
-    ENABLE_EXTRA_VENDOR_LIBS := false
+    ENABLE_EXTRA_VENDOR_LIBS := true
     PRODUCT_PACKAGES += vendor-extra-libs
 endif
 
