d042ba692f53e4478d6eb8e413925d31b8635791
Kripa Bhat <kripbhat@codeaurora.org>
2020-10-15 15:46:55 +0530

    MSM8937_64: Clean-up the no longer supported logdumpd feature.
    Change-Id: I18037137b344c572dfc6178ae568a4bd5cff41ee

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -297,21 +297,6 @@ PRODUCT_PACKAGES += \
  PRODUCT_COPY_FILES += \
      device/qcom/msm8937_64/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-
-# Enable logdumpd service only for non-perf bootimage
-ifeq ($(findstring perf,$(KERNEL_DEFCONFIG)),)
-    ifeq ($(TARGET_BUILD_VARIANT),user)
-        PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-            ro.logdumpd.enabled=0
-    else
-        #PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-            ro.logdumpd.enabled=1
-    endif
-else
-    PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-        ro.logdumpd.enabled=0
-endif
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service \
