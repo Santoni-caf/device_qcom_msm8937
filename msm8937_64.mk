44765d777c1da751424548de5c74c06a2136b6e8
MrArtemSid <artem.sid.key@gmail.com>
2020-12-06 23:49:39 +0400

    msm8937_64: Import santoni keylayout

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -497,6 +497,13 @@ ifeq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),4.19))
 PRODUCT_PACKAGES += init.qti.dcvs.sh
 endif
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_720p.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5x06_720p.kl \
+    $(LOCAL_PATH)/keylayout/gf3208.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gf3208.kl \
+    $(LOCAL_PATH)/keylayout/msm8920-sku7-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8920-sku7-snd-card_Button_Jack.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
+
 ###################################################################################
 # This is the End of target.mk file.
 # Now, Pickup other split product.mk files:
