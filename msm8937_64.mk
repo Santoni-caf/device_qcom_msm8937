d52410a0008ccb75f6603549e198cdd09105faee
MrArtemSid <artem.sid.key@gmail.com>
2020-12-13 10:25:33 +0400

    msm8937_64: Delete soter support, because santoni doesn't support it

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -341,7 +341,7 @@ PRODUCT_PACKAGES += android.hardware.light@2.0-impl
 PRODUCT_PACKAGES += android.hardware.thermal@1.0-impl \
                     android.hardware.thermal@1.0-service
 
-TARGET_SUPPORT_SOTER := true
+TARGET_SUPPORT_SOTER := false
 
 #set KMGK_USE_QTI_SERVICE to true to enable QTI KEYMASTER and GATEKEEPER HIDLs
 ifeq ($(ENABLE_VENDOR_IMAGE), true)
